c8a1f667eadb856235ebe6b67453c33e58e9ae0b
Univa <41708691+Univa@users.noreply.github.com>
2020-05-15 15:24:33 -0400

    simplify css for Char component

--- src/scenes/type/components/Words/Char.jsx
+++ src/scenes/type/components/Words/Char.jsx
@@ -12,7 +12,7 @@ export default class Char extends React.Component {
                         else if (this.props.type === "incorrect") { color = theme.color.incorrect }
                         else if (this.props.type === "notTyped") { color = theme.color.notTyped }
 
-                        return <span className={ this.props.type } style={{color: color}}>{ this.props.character }</span>
+                        return <span className={ "Char " + this.props.type } style={{color: color}}>{ this.props.character }</span>
                     }
                 }
             </SettingsContext.Consumer>
